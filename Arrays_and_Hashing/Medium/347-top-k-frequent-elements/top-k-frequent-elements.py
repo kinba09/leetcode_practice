2a95bcf44cb4321b98c6afcc6312822673c2de4f
Abnik A <113089217+kinba09@users.noreply.github.com>
2025-07-25 11:46:37 +0530

    Time: 14 ms (12.23%) | Memory: 24.5 MB (7.66%) - LeetSync

--- Arrays_and_Hashing/Medium/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ Arrays_and_Hashing/Medium/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,14 +2,30 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #  count = Counter(nums)
         #  return [items for items,freq in count.most_common(k)]
-        arr = Counter(nums)
-        sorted_items = sorted(arr.items(), key=lambda kv: kv[1], reverse=True)
-        ans = []
-        for i in range(k):
-             # sorted_items[i] is (num, freq), so take [0] for the num
-            ans.append(sorted_items[i][0])
-        return ans
+       
+        # arr = Counter(nums)
+        # sorted_items = sorted(arr.items(), key=lambda kv: kv[1], reverse=True)
+        # ans = []
+        # for i in range(k):
+        #      # sorted_items[i] is (num, freq), so take [0] for the num
+        #     ans.append(sorted_items[i][0])
+        # return ans
 
+
+        #bucket_sort
+        freq = Counter(nums)
+        n = len(nums)
+        arr: list[list[int]] = [[] for _ in range(n+1)]
+        for val,num in freq.items():
+            arr[num].append(val)
+        ans = []
+        for i in range(n,0,-1):
+            for val in arr[i]:
+                ans.append(val)
+                if len(ans) == k:
+                    return ans
+        return ans
+        
 
 
 
